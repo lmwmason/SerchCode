192822cd9d9f731bc5d4112c27917492c4f683a9
이무원 <lmwmason@naver.com>
2025-08-27 16:48:57 +0900

    MainUI

--- search_code/src/App.js
+++ search_code/src/App.js
@@ -1,25 +1,55 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const onChange = (e) => {
+    setValue(e.target.value);
+    console.log(value);
+  };
+
   return (
     <div className="App">
+      <div className="menu-title">
+        <a href="#Home">Search Code!</a>
+      </div>
+      <div className="menu-list">
+      <div className="menu-item">
+        <a href="#C">C</a>
+      </div>
+      <div className="menu-item">
+        <a href="#cpp">c++</a>
+      </div>
+      <div className="menu-item">
+        <a href="#Java">Java</a>
+      </div>
+      <div className="menu-item">
+        <a href="#Python">Python</a>
+      </div>
+      <div className="menu-item">
+        <a href="#CS">C#</a>
+      </div>
+      <div className="menu-item">
+        <a href="#Js">Javascript</a>
+      </div>
+      <div className="menu-item">
+        <a href="#Dart">Dart</a>
+      </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Search Code!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="search"
+          placeholder="검색어를 입력하세요!"
+          value={value}
+          onChange={onChange}
+        />
       </header>
     </div>
   );
 }
-
-export default App;
